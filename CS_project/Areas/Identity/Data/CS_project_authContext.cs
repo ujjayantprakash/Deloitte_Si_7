872852cfff67571d928b137ce2e59395085ac841
ujjayant <ujjayant.prakash@learner.manipal.edu>
2022-06-18 16:26:17 +0530

    Add project files.

---
+++ CS_project/Areas/Identity/Data/CS_project_authContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CS_project.Areas.Identity.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace CS_project.Data
+{
+    public class CS_project_authContext : IdentityDbContext<CS_projectUserApplication>
+    {
+        public CS_project_authContext(DbContextOptions<CS_project_authContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            // Customize the ASP.NET Identity model and override the defaults if needed.
+            // For example, you can rename the ASP.NET Identity table names and more.
+            // Add your customizations after calling base.OnModelCreating(builder);
+        }
+    }
+}
---
+++ CS_project/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using CS_project.Areas.Identity.Data;
+using CS_project.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(CS_project.Areas.Identity.IdentityHostingStartup))]
+namespace CS_project.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<CS_project_authContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("CS_project_authContextConnection")));
+
+                services.AddDefaultIdentity<CS_projectUserApplication>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<CS_project_authContext>();
+            });
+        }
+    }
+}
