5a095108ad530b7618aaa5d107d2e64677d1201a
ujjayant <ujjayant.prakash@learner.manipal.edu>
2022-06-23 03:19:45 +0530

    added table layout and worked on ui

--- CS_project/wwwroot/js/site.js
+++ CS_project/wwwroot/js/site.js
@@ -2,8 +2,30 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
-$('submit').click(function () {
-    $(".alert").delay(4000).slideUp(200, function () {
-        $(this).alert('close');
-    })
-});
+//$('#file-upload').click(function () {
+//    $(".alert").delay(4000).slideUp(200, function () {
+//        $(this).alert('close');
+//    })
+//});
+
+//$(document).ready(function () {
+//    $('#label-upload').click(function () {
+//        $('#myalert').show('fade');
+
+
+//        setTimeout(function () {
+//            $('#myalert').hide('fade');
+//        }, 6000);
+
+//    });
+
+//    $('#linkClose').click(function () {
+//        $('#myalert').hide('fade');
+//    });
+//});
+
+var pressedButton = document.getElementById('label-upload');
+pressedButton.addEventListener("click", function (event) {
+    alert("File uploaded successfully");
+})
+
